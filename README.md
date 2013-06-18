4817ea1adec039dc5f12651665e47fad7a0af342
Dr Nic Williams <drnicwilliams@gmail.com>
2013-06-18 14:29:52 -0700

    initial scaffold

---
+++ README.md
@@ -0,0 +1,24 @@
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* cucumber
+* postgresql
+* puma
+* rails_basics
+* redis
+* sidekiq
+* twitter_bootstrap
+* simple_form
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryboshWeb::Application.config.secret_token = '1bc9f6c7e5645515e8413b0b4d7d1997126ddabc33672a0deeab6bb240c9d38d416cfa6116630e75cede53c3b7a419689faa0502599d03beffb4b86c07ad92de'
